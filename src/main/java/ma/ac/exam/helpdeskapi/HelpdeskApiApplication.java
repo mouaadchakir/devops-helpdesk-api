44d62d701f0ab1e0bf6db2203e21f48b70611178
mouad chakir <130808588+mouaadchakir@users.noreply.github.com>
2026-05-01 17:28:45 +0100

    chore: setup Spring Boot and MySQL config

---
+++ src/main/java/ma/ac/exam/helpdeskapi/HelpdeskApiApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.exam.helpdeskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApiApplication.class, args);
+	}
+
+}
